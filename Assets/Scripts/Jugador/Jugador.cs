0b51092d5848067528741eaa763432d21ca81cba
Ale <eze.giorgi@gmail.com>
2019-07-26 11:32:31 -0300

    Controlador del movimiento del jugador

---
+++ Assets/Scripts/Jugador/Jugador.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class Jugador : MonoBehaviour {
+
+    private const float FUERZA_DE_PROPULSION = 100f;
+
+    private Rigidbody2D cuerpo;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private float impulso = 0f;
+    private bool estaCayendo = false;
+    
+    public float velocidadDeVuelo = 25f;
+    public float velocidad = 8f;
+
+
+    public void Start() {
+        cuerpo = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void FixedUpdate() {
+        Caminar();
+        Volar();
+        ComprobarSiEstaCayendo();
+    }
+
+    private void Caminar() {
+
+        float movimientoLateral = Input.GetAxis("Horizontal");
+
+        if (movimientoLateral < 0) {
+            spriteRenderer.flipX = true;
+        } else if(movimientoLateral > 0) {
+            spriteRenderer.flipX = false;
+        }
+        
+        cuerpo.AddForce(new Vector2(movimientoLateral, 0f) * velocidad);
+        animator.SetFloat("movimientoLateral", Mathf.Abs(movimientoLateral));
+    }
+
+    private void Volar() {             
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {    
+            cuerpo.AddForce(Vector2.up * FUERZA_DE_PROPULSION);
+        }
+
+        impulso = Input.GetAxis("Vertical");
+        cuerpo.AddForce(new Vector2(0f, impulso) * velocidadDeVuelo);
+        animator.SetFloat("impulso", impulso);        
+    }
+
+    private void ComprobarSiEstaCayendo() {
+
+        if (cuerpo.velocity.y < 0 && impulso <= 0.1) {
+            estaCayendo = true;
+        } else if (cuerpo.velocity.y >= 0) {            
+            estaCayendo = false;
+        }
+
+        animator.SetBool("cayendo", estaCayendo);
+    }
+
+}
