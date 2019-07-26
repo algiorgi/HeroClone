6210820c8bb678a3730feab09a2db113f1aa406b
Ale <eze.giorgi@gmail.com>
2019-07-26 17:14:33 -0300

    Gemas coleccionables

---
+++ Assets/Scripts/Coleccionables/Gema.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Gema : MonoBehaviour {
+
+    public delegate void AlColeccionar();
+    public static event AlColeccionar Coleccionar;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Coleccionar();
+        Destroy(gameObject);
+    }
+
+
+}
