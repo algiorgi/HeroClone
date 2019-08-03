a37d91f8aff21282f5bca3e97f1347b2baa86901
Ale <eze.giorgi@gmail.com>
2019-08-02 23:00:42 -0300

    El administrador del juego contabiliza la cantidad de prisioneros

---
+++ Assets/Scripts/Administradores/AdministradorDelJuego.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdministradorDelJuego : MonoBehaviour {
+
+    public Transform prisioneros;
+
+    private int cantidadTotalDePrisiones;
+    private int prisionerosRescatados;
+
+    public void Start()  {
+
+        cantidadTotalDePrisiones = prisioneros.childCount;
+        prisionerosRescatados = 0;        
+    }
+
+    private void OnEnable() {
+        Jugador.Rescatar += AlRescatarUnPrisionero;
+    }
+
+    private void OnDisable() {
+        Jugador.Rescatar -= AlRescatarUnPrisionero;
+    }
+
+    private void AlRescatarUnPrisionero() {
+
+        prisionerosRescatados++;
+
+        Debug.Log("Prisionero rescatado: " + prisionerosRescatados + " de " + cantidadTotalDePrisiones);
+    }
+}
